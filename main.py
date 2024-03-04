d03e6265b94b81724886d5d6b0f8324386e68bfb
Sergey Antonov <achiordel@yandex.ru>
2024-03-04 13:31:51 +0300

    Исправления: 1. Добавлен фильтр на фото с текстом; 2. Исправлено форматирование файла Readme.md; 3. Сессии Pyrogram добавлены в исключения;

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import asyncio
 from pyrogram import Client, filters
 
-API_ID = 0000000
+API_ID = 00000000
 API_HASH = ""
 
 app = Client("my_account", API_ID, API_HASH)
 
-@app.on_message(filters.text & filters.channel)
+@app.on_message(filters.text | filters.photo & filters.channel)
 async def main(client, message):
     post = await app.get_discussion_message(message.chat.id, message.id)
     await post.reply("Крутяк!")
